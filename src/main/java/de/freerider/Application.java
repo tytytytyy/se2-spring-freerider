e767cd08a038bb7c5eae065f5818f2961f4a2791
Sven Graupner <sgraupner@beuth-hochschule.de>
2021-06-11 10:49:25 +0200

    @SpringBootApplication: Application.java, HelloController.java

---
+++ src/main/java/de/freerider/Application.java
@@ -0,0 +1,43 @@
+package de.freerider;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+
+@SpringBootApplication
+public class Application {
+
+	Application() {
+		log( "Constructor()" );
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			log( "CommandLineRunner runner()" );
+		};
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	void doWhenApplicationReady() {
+		log( "doWhenApplicationReady()" );
+	}
+
+	public static void main( String[] args ) {
+		log( "main()" );
+		//
+		SpringApplication.run( Application.class, args );
+		//
+		log( "main() leaving" );
+	}
+
+
+	public static void log( String msg ) {
+		System.err.println( Application.class.getSimpleName() + "::" + msg );
+	}
+
+}

b08bbabba2e9377b76cd73e4e51f5b6d86629b57
Tabea <tabea99@hotmail.de>
2021-06-25 23:54:24 +0200

    sure-fire-reports

--- src/test/java/de/freerider/repository/CustomerRepositoryTest.java
+++ src/test/java/de/freerider/repository/CustomerRepositoryTest.java
@@ -1,8 +1,9 @@
 package de.freerider.repository;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
